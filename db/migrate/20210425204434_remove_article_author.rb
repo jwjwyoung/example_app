8e87aec2fc27da0b8272c62cffd14c762fecce39
jw <x1022069@gmail.com>
2021-04-25 15:45:46 -0500

    remove column author from articles

---
+++ db/migrate/20210425204434_remove_article_author.rb
@@ -0,0 +1,5 @@
+class RemoveArticleAuthor < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :articles, :author
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_25_201056) do
+ActiveRecord::Schema.define(version: 2021_04_25_204434) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.text "description"
-    t.string "author"
+    t.string "owner"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
