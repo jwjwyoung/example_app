20e3994b185e9d50d7d122ed23c44f2507f71d7d
junwen <junwen@uchicago.edu>
2021-05-12 14:26:47 -0500

    add a.b query

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all.includes(:comments)
+    @article = Article.first
+    @comments = @article.comments
   end
 end
