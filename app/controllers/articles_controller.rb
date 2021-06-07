cc81fa09fd9035a0f312163109d43cd806244c29
jw <x1022069@gmail.com>
2021-06-06 20:44:43 -0500

    adds more queries

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,17 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
+    @articles = @articles.where("author = ?", params[:author])
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article.title = params[:title]
+    @article.author = params[:author]
+    @article.updated_at = Time.now
+    @article.save
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,10 @@
 class Article < ApplicationRecord
+  validates_presence_of :id
+  validates_presence_of :title
+  valiates_uniqueness_of :id
+  
   def self.has_no_author
+    where(is_deleted: false).
     where(author: nil)
   end
 end
