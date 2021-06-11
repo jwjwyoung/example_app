8a13b077f0d68f6c423c80387ebcce7c0c1e8f3e
jw <x1022069@gmail.com>
2021-06-11 14:18:11 -0500

    update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,13 @@
 class ArticlesController < ApplicationController
-  def index
-    @articles = Article.all
+    def index
+      @articles = Article.all
+      @articles = @articles.where(author: params)
+    end
+    def edit
+      @article.title = title
+      @article.author = params
+      @article.updated_at = Time.now
+      @article.save
+    end
   end
-end
+  
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,9 @@
 class Article < ApplicationRecord
+  validates_presence_of :id
+  validates_presence_of :title
+  valiates_uniqueness_of :id
   def self.has_no_author
     where(author: nil)
   end
 end
+  
