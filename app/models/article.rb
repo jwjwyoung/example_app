b179c11ee45df81df9d4b127637b0da2d2ade53b
jw <x1022069@gmail.com>
2021-07-01 23:47:03 -0500

    add new change

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ApplicationRecord
   validates_presence_of :title
   valiates_uniqueness_of :id
   def self.has_no_author
-    where(author: nil)
+    where(author: nil, title: nil)
   end
 end
   
--- db/migrate/20210425203632_change_author_to_owner.rb
+++ db/migrate/20210425203632_change_author_to_owner.rb
@@ -1,5 +1,6 @@
 class ChangeAuthorToOwner < ActiveRecord::Migration[6.0]
   def change
     rename_column :articles, :author, :owner
+    rename_column :articles, :title, :header
   end
 end
